a311ada8028711ba79950c8025b0a3f900632d96
Koki Yoshida <yoshida_koki@cyberagent.co.jp>
2024-03-19 09:39:51 +0900

    tmp

--- packages/app/lib/ui/screen/slide/content/slide_3.dart
+++ packages/app/lib/ui/screen/slide/content/slide_3.dart
@@ -1,4 +1,5 @@
 import 'package:ca_flutter_slide/foundation/build_context_exe.dart';
+import 'package:ca_flutter_slide/gen/assets.gen.dart';
 import 'package:ca_flutter_slide/ui/screen/slide/component/auto_resized_text.dart';
 import 'package:ca_flutter_slide/ui/screen/slide/component/custom_gap.dart';
 import 'package:ca_flutter_slide/ui/screen/slide/component/custom_slide_builder.dart';
@@ -21,23 +22,35 @@ class Slide3 extends FlutterDeckSlideWidget {
       builder: customSlideBuilder(
         pageNumber: 3,
         title: 'Rive とは？',
-        builder: (context) => Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        builder: (context) => Stack(
           children: [
-            AutoResizedText(
-              'インタラクティブアニメーション*を作成できるツール',
-              textAreaHeight: context.slideSize.height * 0.1,
-              style: context.text.displayLarge?.bold,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AutoResizedText(
+                  'インタラクティブアニメーション*を作成できるツール',
+                  textAreaHeight: context.slideSize.height * 0.1,
+                  style: context.text.displayLarge?.bold,
+                ),
+                const CGap(heightFactor: 0.1),
+                AutoResizedText(
+                  '*ユーザーの操作によって動的に変化するアニメーション\n 例えば、、、',
+                  textAreaHeight: context.slideSize.height * 0.1,
+                  style: context.text.displayMedium,
+                ),
+                Rating(
+                  width: context.slideSize.width * 0.3,
+                  height: context.slideSize.height * 0.2,
+                ),
+              ],
             ),
-            const CGap(heightFactor: 0.1),
-            AutoResizedText(
-              '*ユーザーの操作によって動的に変化するアニメーション\n 例えば、、、',
-              textAreaHeight: context.slideSize.height * 0.1,
-              style: context.text.displayMedium,
-            ),
-            Rating(
-              width: context.slideSize.width * 0.3,
-              height: context.slideSize.height * 0.2,
+            Positioned(
+              bottom: context.slideSize.height * 0.1,
+              right: 0,
+              child: Assets.images.qr.image(
+                width: context.slideSize.width * 0.25,
+                height: context.slideSize.height * 0.25,
+              ),
             ),
           ],
         ),
